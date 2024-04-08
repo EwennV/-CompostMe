e0ac3d21266221c95b90569ce57351f8b8e2749f
Ewenn V <146433402+EwennV@users.noreply.github.com>
2024-04-08 15:23:24 +0200

    8 formulaires composteur (#9)
    * Installation de Symfony Profiler - #7
    
     Please enter the commit message for your changes. Lines starting
    
    * Design de la page de connexion - #7
    
    * Ajout du formulaire de création de composteur

--- src/Entity/Composter.php
+++ src/Entity/Composter.php
@@ -2,12 +2,23 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\ComposterRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ComposterRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post()
+    ],
+)]
 class Composter
 {
     #[ORM\Id]
