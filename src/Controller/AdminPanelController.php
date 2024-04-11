a95bc4f5b2df05b8422bb4885830a7a16c308421
Ewenn V <146433402+EwennV@users.noreply.github.com>
2024-04-11 14:41:42 +0200

    Création de la liste des tickets - #16

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ComposterRepository;
+use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +12,21 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminPanelController extends AbstractController
 {
     #[Route('/panel', name: 'app_admin_panel')]
-    public function index(): Response
+    public function index(TicketRepository $ticketRepository, ComposterRepository $composterRepository): Response
     {
         return $this->render('admin_panel/index.html.twig', [
             'controller_name' => 'AdminPanelController',
+            'tickets' => $ticketRepository->findAll(),
+            'composters' => $composterRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/panel/tickets', name: 'app_admin_panel_tickets')]
+    public function tickets(TicketRepository $ticketRepository): Response
+    {
+        return $this->render('admin_panel/tickets/list.html.twig', [
+            'controller_name' => 'AdminPanelController',
+            'tickets' => $ticketRepository->findAll(),
         ]);
     }
 }
