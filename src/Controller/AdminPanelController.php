17eea18762845fc6774313514a90a6658a3636dc
EwennV <ewenn.vallois.perso@gmail.com>
2024-04-29 13:54:27 +0200

    Ajout des CRUD des paramètres - #29

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\AccessType;
 use App\Entity\FillRateType;
+use App\Entity\OwnerType;
+use App\Form\AccessTypeType;
 use App\Form\FillRateTypeType;
+use App\Form\OwnerTypeType;
+use App\Repository\AccessTypeRepository;
 use App\Repository\ComposterRepository;
 use App\Repository\FillRateTypeRepository;
+use App\Repository\OwnerTypeRepository;
 use App\Repository\TicketRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use phpDocumentor\Reflection\Types\This;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -47,13 +54,19 @@ class AdminPanelController extends AbstractController
 
     #[Route('/panel/parametres', name: 'app_admin_panel_settings')]
     public function settings(
-        FillRateTypeRepository $fillRateTypeRepository
+        FillRateTypeRepository $fillRateTypeRepository,
+        AccessTypeRepository $accessTypeRepository,
+        OwnerTypeRepository $ownerTypeRepository,
     ): Response
     {
         $fillRates = $fillRateTypeRepository->findAll();
+        $accessType = $accessTypeRepository->findAll();
+        $ownerTypes = $ownerTypeRepository->findAll();
 
         return $this->render('admin_panel/settings.html.twig', [
-            'fillRates' => $fillRates
+            'fillRates' => $fillRates,
+            'accessTypes' => $accessType,
+            'ownerTypes' => $ownerTypes,
         ]);
     }
 
@@ -99,7 +112,6 @@ class AdminPanelController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $fillRateType = $form->getData();
 
             $entityManager->flush();
 
@@ -109,6 +121,200 @@ class AdminPanelController extends AbstractController
         }
 
         return $this->render('components/unitedForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/panel/fillrate/delete/{fill_rate_id}', name: 'app_admin_panel_fillrate_delete')]
+    public function deleteFillRateType(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        #[MapEntity(mapping: ['fill_rate_id' => 'id'])] FillRateType $fillRateType
+    )
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('app_admin_panel_fillrate_delete', ['fill_rate_id' => $fillRateType->getId()]))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->remove($fillRateType);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Taux de remplissage supprimé avec succès !');
+
+            return $this->redirectToRoute('app_admin_panel_settings');
+        }
+
+        return $this->render('components/unitedForm.html.twig', [
+            'isDeleteForm' => true,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/panel/accesstype/add', name: 'app_admin_panel_accesstype_add')]
+    public function addAccessType(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $form = $this->createForm(AccessTypeType::class, null, [
+            'action' => $this->generateUrl('app_admin_panel_accesstype_add'),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $accessType = $form->getData();
+
+            $entityManager->persist($accessType);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Type d\'accès ajouté avec succès !');
+
+            return $this->redirectToRoute('app_admin_panel_settings');
+        }
+
+        return $this->render('components/unitedForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/panel/accesstype/edit/{access_type_id}', name: 'app_admin_panel_accesstype_edit')]
+    public function editAccessType(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        #[MapEntity(mapping: ['access_type_id' => 'id'])] AccessType $accessType
+    ): Response
+    {
+        $form = $this->createForm(AccessTypeType::class, $accessType, [
+            'action' => $this->generateUrl('app_admin_panel_accesstype_edit', ['access_type_id' => $accessType->getId()]),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $accessType = $form->getData();
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Type d\'accès modifié avec succès !');
+
+            return $this->redirectToRoute('app_admin_panel_settings');
+        }
+
+        return $this->render('components/unitedForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/panel/accesstype/delete/{access_type_id}', name: 'app_admin_panel_accesstype_delete')]
+    public function deleteAccessType(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        #[MapEntity(mapping: ['access_type_id' => 'id'])] AccessType $accessType
+    ): Response
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('app_admin_panel_accesstype_delete', ['access_type_id' => $accessType->getId()]))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->remove($accessType);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Type d\'accès supprimé avec succès !');
+
+            return $this->redirectToRoute('app_admin_panel_settings');
+        }
+
+        return $this->render('components/unitedForm.html.twig', [
+            'isDeleteForm' => true,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/panel/ownertype/add', name: 'app_admin_panel_ownertype_add')]
+    public function addOwnerType(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $form = $this->createForm(OwnerTypeType::class, null, [
+            'action' => $this->generateUrl('app_admin_panel_ownertype_add'),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ownerType = $form->getData();
+
+            $entityManager->persist($ownerType);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Type de propriétaire ajouté avec succès !');
+
+            return $this->redirectToRoute('app_admin_panel_settings');
+        }
+
+        return $this->render('components/unitedForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/panel/ownertype/edit/{owner_type_id}', name: 'app_admin_panel_ownertype_edit')]
+    public function editOwnerType(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        #[MapEntity(mapping: ['owner_type_id' => 'id'])] OwnerType $ownerType
+    ): Response
+    {
+        $form = $this->createForm(OwnerTypeType::class, $ownerType, [
+            'action' => $this->generateUrl('app_admin_panel_ownertype_edit', ['owner_type_id' => $ownerType->getId()]),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Type de propriétaire modifié avec succès !');
+
+            return $this->redirectToRoute('app_admin_panel_settings');
+        }
+
+        return $this->render('components/unitedForm.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/panel/ownertype/delete/{owner_type_id}', name: 'app_admin_panel_ownertype_delete')]
+    public function deleteOwnerType(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        #[MapEntity(mapping: ['owner_type_id' => 'id'])] OwnerType $ownerType
+    ): Response
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('app_admin_panel_ownertype_delete', ['owner_type_id' => $ownerType->getId()]))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->remove($ownerType);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Type de propriétaire supprimé avec succès !');
+
+            return $this->redirectToRoute('app_admin_panel_settings');
+        }
+
+        return $this->render('components/unitedForm.html.twig', [
+            'isDeleteForm' => true,
             'form' => $form->createView(),
         ]);
     }
