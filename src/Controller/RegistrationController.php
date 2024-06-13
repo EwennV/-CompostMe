e5bb1089cebfa14fd681c16a80568535515f8cb4
EwennV <ewenn.vallois.perso@gmail.com>
2024-06-13 23:54:12 +0200

    Correction de l'envoie de mail #56

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,7 +47,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('noreply@compostandmme.cesi.ewenn-vallois.fr', 'Compost &Me'))
+                    ->from(new Address('compostme+noreply@mail.ewenn-vallois.fr', '[NO REPLY] Compost &Me'))
                     ->to($user->getEmail())
                     ->subject('Confirmer votre adresse mail')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
